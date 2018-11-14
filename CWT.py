6b4882b90ff1c3727ae896a47b8f443fde70165b
Heroin14 <nswarup14@gmail.com>
2018-11-14 11:20:30 +0530

    CWT functionality successfully added

--- CWT.py
+++ CWT.py
@@ -2,12 +2,12 @@
 
 # Form implementation generated from reading ui file 'CWT.ui'
 #
-# Created by: PyQt4 UI code generator 4.11.4
+# Created by: PyQt4 UI code generator 4.12.1
 #
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
-
+import CWT_noise
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
@@ -23,56 +23,49 @@ except AttributeError:
         return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_Form(object):
+
+    def checkPoint(self):
+        msg= QtGui.QMessageBox()
+        #msg.setIcon(QMessageBox.Critical)
+        msg.setText("Please a valid file path")
+        msg.setWindowTitle("MessageBox")
+
+        msg.setStandardButtons(QtGui.QMessageBox.Yes)
+        ret= msg.exec_()
+    
+        if(ret== QtGui.QMessageBox.Yes):
+            return
+
+    def messagePoint(self):
+        
+        try:
+            open(self.lineEdit.text(),"r")
+        except FileNotFoundError as e:
+            self.checkPoint()
+            return
+
+        CWT_noise.main(self.lineEdit.text())
+
     def setupUi(self, Form):
         Form.setObjectName(_fromUtf8("Form"))
         Form.resize(400, 300)
-        self.widget = QtGui.QWidget(Form)
-        self.widget.setGeometry(QtCore.QRect(40, 20, 164, 148))
-        self.widget.setObjectName(_fromUtf8("widget"))
-        self.formLayout = QtGui.QFormLayout(self.widget)
-        self.formLayout.setObjectName(_fromUtf8("formLayout"))
-        self.label = QtGui.QLabel(self.widget)
+        self.label = QtGui.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(40, 100, 151, 17))
         self.label.setObjectName(_fromUtf8("label"))
-        self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.label)
-        self.label_2 = QtGui.QLabel(self.widget)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.label_2)
-        self.gridLayout = QtGui.QGridLayout()
-        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
-        self.pushButton = QtGui.QPushButton(self.widget)
-        self.pushButton.setObjectName(_fromUtf8("pushButton"))
-        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
-        self.label_3 = QtGui.QLabel(self.widget)
-        self.label_3.setObjectName(_fromUtf8("label_3"))
-        self.gridLayout.addWidget(self.label_3, 0, 1, 1, 1)
-        self.pushButton_2 = QtGui.QPushButton(self.widget)
-        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
-        self.gridLayout.addWidget(self.pushButton_2, 1, 0, 1, 1)
-        self.label_4 = QtGui.QLabel(self.widget)
-        self.label_4.setObjectName(_fromUtf8("label_4"))
-        self.gridLayout.addWidget(self.label_4, 1, 1, 1, 1)
-        self.pushButton_3 = QtGui.QPushButton(self.widget)
-        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
-        self.gridLayout.addWidget(self.pushButton_3, 2, 0, 1, 1)
-        self.label_5 = QtGui.QLabel(self.widget)
-        self.label_5.setObjectName(_fromUtf8("label_5"))
-        self.gridLayout.addWidget(self.label_5, 2, 1, 1, 1)
-        self.formLayout.setLayout(2, QtGui.QFormLayout.LabelRole, self.gridLayout)
-
+        self.lineEdit = QtGui.QLineEdit(Form)
+        self.lineEdit.setGeometry(QtCore.QRect(230, 90, 113, 29))
+        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.pushButton= QtGui.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(40,150, 80,27))
         self.retranslateUi(Form)
         QtCore.QMetaObject.connectSlotsByName(Form)
 
     def retranslateUi(self, Form):
         Form.setWindowTitle(_translate("Form", "Form", None))
-        self.label.setText(_translate("Form", "TextLabel", None))
-        self.label_2.setText(_translate("Form", "TextLabel", None))
-        self.pushButton.setText(_translate("Form", "PushButton", None))
-        self.label_3.setText(_translate("Form", "TextLabel", None))
-        self.pushButton_2.setText(_translate("Form", "PushButton", None))
-        self.label_4.setText(_translate("Form", "TextLabel", None))
-        self.pushButton_3.setText(_translate("Form", "PushButton", None))
-        self.label_5.setText(_translate("Form", "TextLabel", None))
+        self.label.setText(_translate("Form", "Enter the path of the file", None))
+        self.pushButton.setText(_translate("Form","Ok",None))
 
+        self.pushButton.clicked.connect(self.messagePoint)
 
 if __name__ == "__main__":
     import sys
--- arima/ar.py
+++ arima/ar.py
@@ -3,14 +3,16 @@ from pandas import read_csv
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 def main():
-	series = read_csv('/home/akshit/Documents/Downloads/2.csv', header=0, index_col=0, squeeze=True)
+	filepath=os.path.abspath(os.curdir) 
+	series = read_csv(filepath+"/2.csv", header=0, index_col=0, squeeze=True)
 	series.columns = ['a', 'b', 'c', 'd']
 	series['e'] = pow((series['a']*series['a'] + series['b']*series['b'] + series['c']*series['c']), 0.5)
 
 	df = pd.DataFrame({'$a': series['e']})
-
+	print(df)
 
 	fd = np.array(df)
 	plt.plot(fd)
@@ -24,3 +26,6 @@ def main():
 	plt.xlabel("Indices",fontsize=12)
 	plt.ylabel("Data",fontsize=12)
 	plt.show()
+
+if __name__ == '__main__':
+	main()
